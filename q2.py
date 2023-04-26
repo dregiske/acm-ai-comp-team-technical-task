16136b3ef4a6a3c39620ec3c6a39350c1aaea4aa
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-04-26 02:04:07 -0700

    Feat: add q2b

--- q2.py
+++ q2.py
@@ -5,11 +5,23 @@ EXPECTED_FILE_NAMES = ['walls', 'terrain']
 CSV_SUFFIX = '.csv'
 CSV_DIR = 'csvs'
 
+EMPTY, DRYWALL, WOOD, STONE = [0, 1, 2, 3]
 MUD, DIRT, STONE, BEDROCK = [0, 1, 2, 3]
+
+LEAK_ORIGIN = (6, 5)
 
 def unstable_walls(walls: np.ndarray, terrain: np.ndarray, threshold: int = MUD) -> int:
 
     #------------------------------------------ YOUR CODE GOES HERE ------------------------------------------
+    # Question 2a
+    
+    return -1
+    #---------------------------------------------------------------------------------------------------------
+
+def leak_territory(walls: np.ndarray, leak_origin: tuple[int] = LEAK_ORIGIN) -> int:
+
+    #------------------------------------------ YOUR CODE GOES HERE ------------------------------------------
+    # Question 2b
 
     return -1
     #---------------------------------------------------------------------------------------------------------
@@ -17,7 +29,7 @@ def unstable_walls(walls: np.ndarray, terrain: np.ndarray, threshold: int = MUD)
 def validate_env(csv_dir: Path):
     
     if (not csv_dir.exists()):
-        raise('eps or jpg dir does not exist')
+        raise('csv dir does not exist')
     for expected_file in EXPECTED_FILE_NAMES:
         expected_file_path = csv_dir / (expected_file + CSV_SUFFIX)
         if (not expected_file_path.exists()):
@@ -32,7 +44,11 @@ def main():
     WALLS = np.loadtxt(CSV_DIR / Path('walls.csv'), delimiter=',').astype(np.int8)
     TERRAIN = np.loadtxt(CSV_DIR / Path('terrain.csv'), delimiter=',').astype(np.int8)
 
-    print(unstable_walls(WALLS, TERRAIN, threshold=DIRT))
+    # Q2a result printed here
+    print(unstable_walls(np.copy(WALLS), np.copy(TERRAIN), threshold=DIRT))
+
+    # Q2b result printed here
+    print(leak_territory(np.copy(WALLS), leak_origin=LEAK_ORIGIN))
 
 if __name__ == '__main__':
     main()
